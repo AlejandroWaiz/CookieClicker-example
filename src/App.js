7309f52bd9fcf674bafc31c028aabcbaef15a3fd
Alejandro Waiz <alejosaurio1@gmail.com>
2023-06-01 01:55:58 -0400

    First push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useReducer } from 'react';
 
-function App() {
+import ClicksReducer from './reducers/ClicksReducer'
+import Clicker from './components/Clicker/Clicker';
+import Autoclicker from './components/AutoClickers/AutoClickers';
+
+import { initialState } from './InitialState';
+
+const App = () => {
+  const [state, dispatch] = useReducer(ClicksReducer, initialState);
+  const callback = useRef();
+
+  useEffect(() => {
+    callback.current = () => {
+      const currentClics = state.clicks.amount;
+      const totalCount = Object.keys(state)
+        .reduce(
+          (acc, cur, idx) => idx === 0
+            ? acc
+            : acc + state[cur].amount * (state[cur].cost * 0.1),
+          currentClics,
+        );
+
+      dispatch({ type: 'update', payload: totalCount });
+    };
+  }, [state]);
+
+  useEffect(() => {
+    const interval = setInterval(() => callback.current(), 1000);
+    return () => clearInterval(interval);
+  }, [callback]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="banner">
+        Original example can found in:
+        <a href="https://github.com/Alaricus/clicker-tutorial-react"> GitHub</a>
+
+      </div>
+      <Clicker amount={state.clicks.amount} dispatch={dispatch} />
+      {
+        Object.keys(state).map((tier, idx) => {
+          if (idx === 0) { return null; }
+          const { cost, amount } = state[tier];
+          return (state.clicks.amount >= cost || amount > 0) && (
+            <Autoclicker
+              key={tier}
+              tier={tier}
+              amount={amount}
+              cost={cost}
+              enabled={state.clicks.amount >= cost}
+              dispatch={dispatch}
+            />
+          );
+        })
+      }
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Clicker/Clicker.js
@@ -0,0 +1,17 @@
+import "./Clicker.css"
+
+const Clicker = ({ amount, dispatch }) => (
+    <div className="clicker">
+      <h1>{Intl.NumberFormat().format(amount)}</h1>
+      <button
+        className="buy"
+        type="button"
+        onClick={() => dispatch({ type: 'click' })}
+      >
+        click button
+      </button>
+    </div>
+  );
+  
+  export default Clicker;
+  
